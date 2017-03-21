f5e19399c6aae8237eb004c95e905b9259d3ea30
Janita <zhuchenjian@huizhi-tech.com>
2017-03-21 19:47:36 +0800

    Initial commit

---
+++ neo4j-one/src/main/java/com/janita/neo4j/one/Neo4jOneApplication.java
@@ -0,0 +1,15 @@
+package com.janita.neo4j.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017-03-21 19:45
+ */
+@SpringBootApplication
+public class Neo4jOneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jOneApplication.class, args);
+    }
+    
+}
